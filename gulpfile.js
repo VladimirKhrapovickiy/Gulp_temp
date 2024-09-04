fd7a5a4f9a54fd8d18bdfc661bb246e1a103b826
VladimirKhrapovickiy <vovandossser90@gmail.com>
2024-09-04 11:54:13 +0700

    some module fix and gulp.js refact

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,76 @@
-let preprocessor = 'sass'
-const gulp = require('gulp')
-const { src, dest, parallel, series, watch } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync').create();
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const cleancss = require('gulp-cleaner-css');
-const imagecomp = require('compress-images');
-const clean = require('gulp-clean');
-const purgecss = require('gulp-purgecss')
 
-function browsersync() {//сервер с автообновлением
-	browserSync.init({ 
-		server: { baseDir: 'app/' }, // путь до файлов
-		notify: false, // уведомления
-		online: true // Режим работы
-	})
-}
 
-function scripts() {
-	return src([ 
-		'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
-		'app/js/main.js', // родные скрипты, идут после библиотек!!!!
-		])
-	.pipe(concat('main.min.js')) // Сборка в 1 файл
-	.pipe(uglify()) // Сжатие
-	.pipe(dest('dist/js/')) // Куда выгружать собранные скрипты
-	.pipe(browserSync.stream()) // Запускаем обновление страницы
-}
-function styles() {
-	return src('app/' + preprocessor + '/main.scss') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
-	.pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
-	.pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
-	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
-	.pipe(dest('dist/css/')) // Выгрузим результат в папку "app/css/"
-	.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
-}
 
-async function images() {
-	imagecomp(
-		"app/img/src/**/*", // Берём все изображения из папки источника
-		"app/img/dest/", // Выгружаем оптимизированные изображения в папку назначения
-		{ compress_force: false, statistic: true, autoupdate: true }, false, // Настраиваем основные параметры
-		{ jpg: { engine: "mozjpeg", command: ["-quality", "75"] } }, // Сжимаем и оптимизируем изображеня
-		{ png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
-		{ svg: { engine: "svgo", command: "--multipass" } },
-		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
-		function (err, completed) { // Обновляем страницу по завершению
-			if (completed === true) {
-				browserSync.reload()
-			}
-		}
-	)
-}
-function cleanimg() {
-	return src('app/img/dest/', {allowEmpty: true}).pipe(clean()) // Удаляет папку "app/images/dest/"
-}
+gulp.task('server', function() {
 
-function startwatch() {
-	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
-    watch('app/**/' + preprocessor + '/**/*', styles);
-    watch('app/**/*.html').on('change', browserSync.reload);
-    watch('app/img/src/**/*', images);
-}
-function buildcopy() {
-	return src([ // Выбираем нужные файлы
-		'app/img/dest/**/*',
-		'app/**/*.html',
-		], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
-	.pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
-}
-function cleandist() {
-	return src('dist', {allowEmpty: true}).pipe(clean()) // Удаляем папку "dist/"
-}
-function purgeCss() { //чистка неиспользуемого css
-    return src('dist/**/*.min.css')
-        .pipe(purgecss({
-            content: ['dist/**/*.html']
-        }))
-        .pipe(gulp.dest('dist/'))
-}
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
 
-//задачи
-    exports.purgecss = purgeCss;
-    exports.cleanimg = cleanimg;
-    exports.images = images;
-    exports.styles = styles;
-    exports.browsersync = browsersync;
-    exports.scripts = scripts;
-    exports.default = parallel(styles, scripts, browsersync, startwatch, purgeCss);
-    exports.build = series(cleandist, styles, scripts, images, buildcopy);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/img/**", gulp.parallel('images'))
+});
+
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        // .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([ 
+		'node_modules/jquery/dist/jquery.min.js' ,'src/js/**/*.js'])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
+
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*",{ encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img/"));
+});
+
+
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
   
